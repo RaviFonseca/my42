ae6815f79170b3a86e2a771b45946d7b37cb2f45
RaviFonseca <ravi.fonseca8@gmail.com>
2026-04-09 00:43:54 +0200

    my first commit

---
+++ myLib.c
@@ -0,0 +1,92 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int     ft_strlen(char *str)
+{
+        int     i;
+        i = 0;
+        while (str[i] != '\0')
+                i++;
+        return (i);
+}
+
+char 	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+	i = 0;
+	while ( src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+void    ft_putchar(char c){
+
+        write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+void    ft_putnbr(int nb){
+        if (nb < 0){
+                nb *= (-1);
+                write (1, "-",1);
+        }
+
+        if (nb >= 10)
+                ft_putnbr(nb/10);
+
+        char c = (nb %10) + '0';
+        write(1, &c, 1);
+}
+
+void	ft_swap_indx(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+char    *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+        int     i;
+        int     j;
+
+        i = 0;
+        j = 0;
+        while (dest[i] != '\0')
+                i++;
+        while (j<nb && src[j] != '\0')
+        {
+                dest[i + j] = src[j];
+                j++;
+        }
+        dest[i + j] = '\0';
+        return (dest);
+
+}
+
+int     ft_strcmp(char *s1, char *s2)
+{
+        int i;
+        i = 0;
+
+        while ( (s1[i] == s2[i]) && s1[i])
+                i++;
+        return ( s1[i] - s2[i]);
+}
---
+++ myLib.h
@@ -0,0 +1,13 @@
+#ifndef "MYLIB_H"
+#define "MYLIB_H"
+
+int     ft_strlen(char *str);
+char	*ft_strcpy(char *dest, char *src);
+void    ft_putchar(char c);
+void	ft_putstr(char *str);
+void    ft_putnbr(int nb);
+void    ft_swap_indx(int *a, int *b);
+char    *ft_strncat(char *dest, char *src, unsigned int nb);
+int     ft_strcmp(char *s1, char *s2);
+
+#endif
