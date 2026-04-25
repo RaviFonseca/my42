b997bfc7abb26898c345f99430f9f537f2c6264e
Raviliano Fonseca <rfonseca@a3-r2-p10.s19.be>
2026-04-25 22:09:52 +0200

    add memchr and memmove

---
+++ libft/srcs/ft_memchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 20:31:46 by rfonseca          #+#    #+#             */
+/*   Updated: 2026/04/25 22:05:44 by rfonseca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	char			nc;
+	size_t			i;
+	unsigned char	*s;
+
+	i = 0;
+	s = (unsigned char *) str;
+	nc = (unsigned char ) c;
+	while (i < n)
+	{
+		if (s[i] == nc)
+			return ((void *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char myStr[] = "Hello World!";
+  char *myPtr = (char*)ft_memchr(myStr, 'W', 12);
+  if (myPtr != NULL) {
+    printf("%s", myPtr);
+  }
+  return 0;
+}*/
---
+++ libft/srcs/ft_memmove.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/25 17:07:24 by rfonseca          #+#    #+#             */
+/*   Updated: 2026/04/25 22:06:32 by rfonseca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*mdest;
+	const char	*msrc;
+	size_t		i;
+
+	mdest = (char *) dest;
+	msrc = (const char *) src;
+	if (dest <= src)
+	{
+		i = 0;
+		while (i < n)
+		{
+			mdest[i] = msrc[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			mdest[i] = msrc[i];
+		}
+	}
+	return (dest);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char dest[] = "oldstring";
+	char src[] = "newstring";
+	ft_memmove(dest, src, 9);
+	printf("%s\n",dest);
+	return (0);
+}*/
