eb33cd16b0baa2e3652f2693a72a93a0aab0c70d
RaviFonseca <ravi.fonseca8@gmail.com>
2026-04-26 21:00:45 +0200

    added memcmp and strdup

---
+++ libft/srcs/ft_memcmp.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfonseca <rfonseca@student.42belgium.be>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/26 16:52:09 by rfonseca          #+#    #+#             */
+/*   Updated: 2026/04/26 17:26:27 by rfonseca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*ms1;
+	unsigned char	*ms2;
+	size_t			i;
+
+	if (n == 0)
+		return (0);
+	ms1 = (unsigned char *)s1;
+	ms2 = (unsigned char *)s2;
+	i = 0;
+	while ((i < n) && (ms1[i] == ms2[i]))
+		i++;
+	return (ms1[i] - ms2[i]);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main () 
+{
+   char str1[15];
+   char str2[15];
+   int ret;
+   int ret2;
+   memcpy(str1, "abcdef", 6);
+   memcpy(str2, "ABCDEF", 6);
+
+   ret = memcmp(str1, str2, 0);
+   ret2 = ft_memcmp(str1, str2, 0);
+   printf("ret: %d\n",ret);
+   printf("ft ret2: %d\n",ret2);
+
+   return(0);
+}*/
---
+++ libft/srcs/ft_strdup.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 17:18:27 by rfonseca          #+#    #+#             */
+/*   Updated: 2026/04/26 19:08:34 by rfonseca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*s1;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s);
+	s1 = (char *)malloc(len + 1);
+	if (!s1)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		s1[i] = s[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *str = "Helloworld";
+    char *result;
+    
+    result = ft_strdup(str);
+    if (result != NULL) {
+        printf("Original string: %s\n", str);
+        printf("Duplicated string: %s\n", result);
+        free(result);  // Always free allocated memory
+    } else {
+        printf("Memory allocation failed\n");
+    }
+    
+    return 0;
+}*/
