c116136a1350e18223ee44ac2fe233ef2d364d21
Raviliano Fonseca <rfonseca@a3-r2-p10.s19.be>
2026-04-28 17:21:55 +0200

    trabalho com a francine

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 15:44:22 by rfonseca          #+#    #+#             */
-/*   Updated: 2026/04/18 19:44:39 by rfonseca         ###   ########.fr       */
+/*   Updated: 2026/04/28 17:13:23 by rfonseca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void	ft_memcpy(void *dest, const *src, size_t n);
+void	ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
@@ -32,9 +32,9 @@ char	*ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *str, int ch);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strrchr(char *str, char *substr);
-char	*ft_strnstr(const char dest, const char src, size_t len);
+size_t	*ft_strnstr(const char dest, const char src, size_t len);
 int		ft_strcmp(const char *str1, const char *str2);
-int		ft_strcncmp(const *str1, const char *str2, size_t n);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
