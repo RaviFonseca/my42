6fbcf6182df6ea3545af8bf87f0138e63d6158b8
RaviFonseca <ravi.fonseca8@gmail.com>
2026-05-03 21:38:23 +0200

    last hour problems with atoi and libft

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rfonseca <rfonseca@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 14:23:32 by rfonseca          #+#    #+#             */
-/*   Updated: 2026/05/03 20:53:12 by rfonseca         ###   ########.fr       */
+/*   Updated: 2026/05/03 21:24:06 by rfonseca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	ft_atoi(const char *str)
 {
 	int	i;
-	int val;
+	int	val;
 
 	i = 0;
 	val = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		val  = val *10 + ( str[i] - 48);
+		val = val * 10 + (str[i] - 48);
 		i++;
 	}
 	return (val);
 }
-
+/*
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -37,4 +37,4 @@ int main(void)
    str = "15546.10E";
    val = ft_atoi(str); 
    printf("integral number = %d\n", val);
-}
+}*/
