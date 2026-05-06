b5f7467c2fd28250179145b0cf2bef8ec829f982
Raviliano Fonseca <rfonseca@a3-r2-p10.s19.be>
2026-05-06 19:10:07 +0200

    update do libft

--- libft/srcs/ft_strlcpy.c
+++ libft/srcs/ft_strlcpy.c
@@ -1,43 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/27 16:08:33 by rfonseca          #+#    #+#             */
-/*   Updated: 2026/04/27 17:02:02 by rfonseca         ###   ########.fr       */
+/*   Created: 2026/05/06 17:38:57 by rfonseca          #+#    #+#             */
+/*   Updated: 2026/05/06 19:04:27 by rfonseca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
-char	*ft_strncpy(char *dest, const char *src, size_t n)
+size_t	ft_strlcpy(char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	size_t	len_s2;
 
+	len_s2 = ft_strlen(s2);
 	i = 0;
-	while ((i < n) && (src[i] != '\0'))
+	if (n > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while ((i < n - 1) && s2[i])
+		{
+			s1[i] = s2[i];
+			i++;
+		}
+		s1[i] = '\0';
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (len_s2);
 }
 /*
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char s1[] = "Hello, Geeks!";
-
-    // Ensure destination has enough space
-    char s2[50];
-
-    // Copying string using strcpy
-    ft_strncpy(s2, s1,5);
-
-    printf("%s", s2);
-    return 0;
+int main()
+{
+	char s1[50];
+	char s2[50] = "hello world!";
+	size_t len = ft_strlcpy(s1,s2,5);
+	printf("%ld  %s\n",len,s1);
+	return(0);
 }*/
--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: rfonseca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 15:44:22 by rfonseca          #+#    #+#             */
-/*   Updated: 2026/05/04 18:11:24 by rfonseca         ###   ########.fr       */
+/*   Updated: 2026/05/06 17:05:45 by rfonseca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,14 @@
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
-char	*ft_strcpy(char *dest, const char *src);
-char	*ft_strncpy(char *dest, const char *src, size_t n);
-char	*ft_strcat(char *dest, const char *src);
-char	*ft_strncat(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *str, int ch);
 char	*ft_strrchr(char *str, int chr);
-char	*ft_strstr(const char *s1, const char *s2);
 char	*ft_strnstr(const char *dest, const char *src, size_t len);
-int		ft_strcmp(const char *str1, const char *str2);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
@@ -43,7 +36,6 @@ int		ft_isprint(int c);
 int		ft_toupper(int ch);
 int		ft_tolower(int c);
 
-void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strdel(char **as);
 void	ft_strclr(char *s);
